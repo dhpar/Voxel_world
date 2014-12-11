ffaff2f1ec14af8d0cacedba25507d32485fb16a
David H. Parramon <davhern@gmail.com>
2014-12-11 10:08:16 -0600

    first commit

---
+++ android/src/com/mygdx/VoxelTest/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.VoxelTest.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.VoxelTest.VoxelTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VoxelTest(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/VoxelTest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.VoxelTest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.VoxelTest.VoxelTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VoxelTest(), config);
+	}
+}
